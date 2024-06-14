f0c364817dca6e536b4adac7271a6b5420475506
adrianismu <adrianismu263@gmail.com>
2024-06-14 23:12:50 +0700

    Edit files

--- app.js
+++ app.js
@@ -1,14 +1,32 @@
+require('dotenv').config();
 const express = require('express');
-require('dotenv').config();
-const app = express();
+const bodyParser = require('body-parser');
+const cors = require('cors');
+const fileUpload = require('express-fileupload');
+const predictRoutes = require('./routes/predictRoutes');
+const loadModel = require('./services/loadModel');
+const errorHandlingMiddleware = require('./middlewares/errorHandlingMiddleware');
 const authRoutes = require('./routes/auth');
-require('dotenv').config();
 
+const app = express();
 app.use(express.json());
-app.use('/auth', authRoutes);
+app.use(cors());
+app.use(bodyParser.json());
+app.use(fileUpload());
 
 const PORT = process.env.PORT || 8081;
 
-app.listen(PORT, () => {
-  console.log(`Server is running on port ${PORT}`);
-});
+
+(async () => {
+  const model = await loadModel();
+  app.locals.model = model;
+  
+  app.use('/auth', authRoutes);
+  app.use('/predict', predictRoutes);
+
+  app.use(errorHandlingMiddleware);
+
+  app.listen(PORT, () => {
+    console.log(`Server started at: http://localhost:${PORT}`);
+  });
+})();
